43754022e91d408f2d2d7f30187cf4e1540c35ae
Siwis06-NoOne <145355790+Siwis06-NoOne@users.noreply.github.com>
2025-02-19 14:02:07 +0100

    Doors

--- Assets/Scripts/PlayerMovementScripts/ThirdPersonCam.cs
+++ Assets/Scripts/PlayerMovementScripts/ThirdPersonCam.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class ThirdPersonCam : MonoBehaviour
 {
+
     [Header("Refrences")]
     [SerializeField] Transform orientation;
     [SerializeField] Transform player;
@@ -11,12 +13,19 @@ public class ThirdPersonCam : MonoBehaviour
     [SerializeField] Transform cameraTransform;
     [SerializeField] Rigidbody myRb;
 
+    [Header("Door Stuf")]
+    [SerializeField] int amountOfKeys;
+    [SerializeField] int currentKeys;
+    [SerializeField] bool hasAllKeys;
+
     [SerializeField] float rotationspeed;
 
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        currentKeys = 0;
+        hasAllKeys = false;
     }
 
     private void Update()
@@ -36,6 +45,26 @@ public class ThirdPersonCam : MonoBehaviour
             Quaternion targetRotation = Quaternion.LookRotation(inputDir.normalized);
             PlayerObj.rotation = Quaternion.Slerp(PlayerObj.rotation, targetRotation, Time.deltaTime * rotationspeed);
         }
-    }   
+
+        if (currentKeys >= amountOfKeys)
+        {
+            hasAllKeys = true;
+        }
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Key")
+        {
+            currentKeys = currentKeys + 1;
+            collision.gameObject.SetActive(false);
+        }
+
+        if (collision.gameObject.tag == "Door" && hasAllKeys == true)
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
 
 }
--- Assets/Scripts/SceneControler.cs
+++ Assets/Scripts/SceneControler.cs
@@ -18,4 +18,6 @@ public class SceneControler : MonoBehaviour
     {
         SceneManager.LoadScene(scene);
     } 
+
+
 }
